0d5d8b63b13157651a27973aa57b8a76af4117ba
Nikola <nikola93n@gmail.com>
2022-02-28 17:01:17 +0100

    First Day - Arrays, Assoc arrays

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php  
+        $numberList = array(253,64,267,478,167,8765,345,'345','<h1>Hello</h1>');
+
+        //$numberList = [];
+
+        // print_r($numberList);
+
+        echo $numberList[8];
+    ?>
+
+</body>
+</html>
