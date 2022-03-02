fcc447ff029face15d7d1fbf428c07d85b3abded
Nikola <nikola93n@gmail.com>
2022-03-02 17:24:23 +0100

    3 Dan - switch statments, while loop, for loop

--- comparison_operators.php
+++ comparison_operators.php
@@ -7,8 +7,30 @@
     <title>Document</title>
 </head>
 <body>
+
+    <!-- <h2>Comparison Operators</h2>
+    <pre>
+        equal ==
+        identical ===
+        compare > <    >=   <=   <>
+        not equal !=
+        not identical !==
+    </pre>
+
+    <h2>Logical Operators</h2>
+    <pre>
+        And &&
+        Or ||
+        Not !
+    </pre> -->
+    
     <?php 
-        
+        if(4 != 5 && 5 < 10 ) {
+            echo "it is true";
+        }
     ?>
+
+
+ 
 </body>
 </html>
---
+++ switch_statments.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+    $number = 10;
+
+    switch ($number) {
+        case 34:
+        echo "it is 34";
+        break;
+        case 37:
+        echo "it is 34";
+        break;
+        case 35:
+        echo "it is 34";
+        break;
+        case 24:
+        echo "it is 24a";
+        break;
+
+        default :
+          echo "we could not find anything";
+          break;
+    }
+        
+    ?>
+</body>
+</html>
