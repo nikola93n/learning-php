28168777659f6b82dff8fdf3d05535d4e784c6bf
Nikola <nikola93n@gmail.com>
2022-03-04 17:05:49 +0100

    Day 4 - Foreach loop

---
+++ foreach.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php  
+    /*
+        $numbers = array(345, 391, 676, 2657, 5784);
+
+        foreach($numbers as $numbers){
+            echo $numbers . "<br>";
+        }
+        */
+    ?>
+    
+    <?php
+        // if()
+    ?>
+
+</body>
+</html>
