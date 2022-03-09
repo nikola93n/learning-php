e22c3be9c43cfab135bc8a00e4d0411403f8d29c
Nikola <nikola93n@gmail.com>
2022-03-09 16:42:51 +0100

    CRUD SQL

---
+++ mysql/login_update.php
@@ -0,0 +1,58 @@
+<?php include "db.php";      
+        
+        $query = "SELECT * FROM users";
+        $result = mysqli_query($connection, $query);
+         if(!$result) {
+             die('Query FAILED' . mysqli_error());
+         }            
+  
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6">
+                <form action="login_create.php" method="post">
+                    <div class="form-gruop mb-3">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" class="form-control">
+                    </div>
+                    <div class="form-gruop mb-3">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control">
+                    </div>
+                    <div class="form-group mb-3">
+                        <select name="" id="">
+                            <option value="">1</option>
+                        </select>
+                    </div>
+                    <input class="btn btn-primary" type="submit" value="UPDATE" name="submit">
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
